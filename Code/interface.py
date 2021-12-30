ebce9fac940f2a466b2187311519689d651eb304
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2021-12-30 15:50:45 +0530

    Added basic layout

--- Code/interface.py
+++ Code/interface.py
@@ -1,8 +1,37 @@
 import sys
-from PyQt5 import QtGui, QtWidgets
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 app: QtWidgets.QApplication = None
 window: QtWidgets.QWidget = None
+
+
+def create_interface():
+    layout = QtWidgets.QHBoxLayout()
+
+    # ---------------------------------------------------------------------------------------
+    # Left Side Layout
+    # ---------------------------------------------------------------------------------------
+    left_side_layout = QtWidgets.QVBoxLayout()
+    left_side_layout.setAlignment(QtCore.Qt.AlignTop)
+    # ---- Elements
+    left_side_layout.addWidget(QtWidgets.QPushButton("Hello World"))
+    left_side_layout.addWidget(QtWidgets.QLineEdit())
+
+    # ---------------------------------------------------------------------------------------
+    # Right Side Layout
+    # ---------------------------------------------------------------------------------------
+    right_side_layout = QtWidgets.QVBoxLayout()
+    right_side_layout.addWidget(QtWidgets.QPushButton("Hello World"))
+    # ---- Elements
+    right_side_layout.setAlignment(QtCore.Qt.AlignTop)
+
+    # ---------------------------------------------------------------------------------------
+    # Final Layout
+    # ---------------------------------------------------------------------------------------
+    layout.addLayout(left_side_layout)
+    layout.addLayout(right_side_layout)
+
+    window.setLayout(layout)
 
 
 def create_window():
@@ -28,4 +57,5 @@ def start_gui():
     global app
     app = QtWidgets.QApplication(sys.argv)
     create_window()
+    create_interface()
     sys.exit(app.exec_())
--- Code/main.py
+++ Code/main.py
@@ -2,14 +2,14 @@ import interface
 
 
 def main():
-    # media = editor.prepare_media(
-    #     "Content/video.mp4",
-    #     "Content/audio.mp3",
-    #     "Content/content.txt",
-    # )
-    # reel = editor.create_reel(media)
-    # editor.export_reel(reel)
-    interface.start_gui()
+    media = editor.prepare_media(
+        "Content/video.mp4",
+        "Content/audio.mp3",
+        "Content/content.txt",
+    )
+    reel = editor.create_reel(media)
+    editor.export_reel(reel)
+    # interface.start_gui()
 
 
 main()
